c9bce14486632a952c2b0b78160a80f0a8edd958
Linda Wandji Lando <lindawandjilando@MacBook-Pro-di-Linda.local>
2025-03-13 21:59:41 +0100

    Rimosso il repository interno a esercitazione_1 e aggiunto correttamente

---
+++ esercitazione_1/fast_trigo.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include "fast_trigo.h"
+
+
+float fast_sin(float x) {
+    return sinf(x);  
+}
+
+
+float fast_cos(float x) {
+    return cosf(x);  
+}
+
+
---
+++ esercitazione_1/fast_trigo.h
@@ -0,0 +1,12 @@
+#ifndef FAST_TRIGO_H
+#define FAST_TRIGO_H
+
+
+float fast_sin(float x);
+float fast_cos(float x);
+
+#endif 
+
+
+
+
